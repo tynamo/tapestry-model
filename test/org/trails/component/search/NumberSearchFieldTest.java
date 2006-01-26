1b2d3c0beba697908f3f8cedd89410d57c10d6a7
lvonk <lvonk@unknown.com>
2006-01-26 20:37:32 +0000

    Added dummy test method to prevent build from failing.

--- test/org/trails/component/search/NumberSearchFieldTest.java
+++ test/org/trails/component/search/NumberSearchFieldTest.java
@@ -4,5 +4,10 @@ import junit.framework.TestCase;
 
 public class NumberSearchFieldTest extends TestCase
 {
-
+	/*
+	 * Created test method otherwise the build failes...
+	 */
+	public void testEmpty() {
+		
+	}
 }
