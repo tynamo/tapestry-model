01e6c02d2a7fc86aa9e726246b29dbec04362ca0
cnelson <cnelson@unknown.com>
2006-03-16 02:55:34 +0000

    refactoring config files to classpath

--- test/org/trails/component/AssociationSelectTest.java
+++ test/org/trails/component/AssociationSelectTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
 import org.jmock.cglib.Mock;
+import org.odmg.DSet;
+import org.trails.descriptor.DescriptorService;
 import org.trails.descriptor.IPropertyDescriptor;
 import org.trails.descriptor.IdentifierDescriptor;
 import org.trails.descriptor.TrailsClassDescriptor;
@@ -21,19 +23,33 @@ public class AssociationSelectTest extends ComponentTest
         // TODO Auto-generated constructor stub
     }
     
-    public void testBuildSelectionModel()
+    TrailsClassDescriptor classDescriptor;
+    IPropertyDescriptor associationDescriptor;
+    AssociationSelect associationSelect;
+    
+    public void setUp() throws Exception
     {
-        Mock psvcMock = new Mock(PersistenceService.class);
-        List instances = new ArrayList();
-        TrailsClassDescriptor classDescriptor = new TrailsClassDescriptor(Foo.class);
+
+    	classDescriptor = new TrailsClassDescriptor(Foo.class);
         classDescriptor.getPropertyDescriptors().add(new IdentifierDescriptor(Foo.class, "id", Integer.class));
-        psvcMock.expects(atLeastOnce()).method("getInstances").with(isA(DetachedCriteria.class)).will(returnValue(instances));
-        AssociationSelect associationSelect = (AssociationSelect)creator.newInstance(AssociationSelect.class, 
-                new Object[] {"persistenceService", psvcMock.proxy()});
-        IPropertyDescriptor associationDescriptor = new TrailsPropertyDescriptor(Foo.class, "foo", Foo.class);
-        associationSelect.setClassDescriptor(classDescriptor);
+    	descriptorServiceMock.expects(atLeastOnce()).method("getClassDescriptor").with(eq(Foo.class)).will(returnValue(classDescriptor));
+ 
+        associationDescriptor = new TrailsPropertyDescriptor(Foo.class, "foo", Foo.class);
+        associationSelect = (AssociationSelect)creator.newInstance(AssociationSelect.class, 
+                new Object[] {
+        		"persistenceService", persistenceMock.proxy(),
+        		"descriptorService", descriptorServiceMock.proxy()
+        });
         associationSelect.setPropertyDescriptor(associationDescriptor);
         associationSelect.setAllowNone(true);
+  	
+    }
+
+    public void testBuildSelectionModel()
+    {
+        
+        List instances = new ArrayList();
+        persistenceMock.expects(atLeastOnce()).method("getInstances").with(isA(DetachedCriteria.class)).will(returnValue(instances));
         associationSelect.buildSelectionModel();
         IdentifierSelectionModel selectionModel = (IdentifierSelectionModel)associationSelect.getPropertySelectionModel();
         assertEquals(1, selectionModel.getOptionCount());
@@ -48,5 +64,10 @@ public class AssociationSelectTest extends ComponentTest
         selectionModel = (IdentifierSelectionModel)associationSelect.getPropertySelectionModel();
         assertEquals("Any", selectionModel.getNoneLabel());
         
+    }
+    
+    public void testGetClassDescriptor()
+    {
+    	assertEquals(classDescriptor, associationSelect.getClassDescriptor());
     }
 }
---
+++ test/org/trails/component/search/AssociationSearchTest.java
@@ -0,0 +1,11 @@
+package org.trails.component.search;
+
+import org.trails.component.ComponentTest;
+
+public class AssociationSearchTest extends ComponentTest
+{
+	public void testBuildCriterion()
+	{
+		
+	}
+}
