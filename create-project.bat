f85ce89c6d160183af6656797f9616cf512ccd07
Alejandro Scandroli <ascandroli@codehaus.org>
2006-11-01 12:46:05 +0000

    fixed TRF-8 and TRF-9

--- create-project.bat
+++ create-project.bat
@@ -1,1 +1,1 @@
-mvn archetype:create -DarchetypeGroupId=org.trails -DarchetypeArtifactId=trails-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=%1 -DartifactId=%2
+mvn archetype:create -DarchetypeGroupId=org.trailsframework -DarchetypeArtifactId=trails-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=%1 -DartifactId=%2
