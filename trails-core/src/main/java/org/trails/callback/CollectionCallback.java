ee98db19428068a5f1f00dac1bf10cac2e3a4428
nhhockeyplayer <nhhockeyplayer@unknown.com>
2007-02-16 18:58:55 +0000

    Added case logic for collection parent/child relationship for save/merge
    Collected entities can now be added, deleted, un-hitched and re-hitched

--- trails-core/src/main/java/org/trails/callback/CollectionCallback.java
+++ trails-core/src/main/java/org/trails/callback/CollectionCallback.java
@@ -2,18 +2,19 @@
  * Created on Feb 28, 2005
  *
  * Copyright 2004 Chris Nelson
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
 package org.trails.callback;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import ognl.Ognl;
 import ognl.OgnlException;
@@ -29,9 +30,9 @@ import org.trails.persistence.PersistenceService;
 public class CollectionCallback extends EditCallback
 {
     private CollectionDescriptor collectionDescriptor;
-	
-	private boolean childRelationship;
-    
+
+    private boolean childRelationship;
+
     /**
      * @param pageName
      * @param model
@@ -45,21 +46,25 @@ public class CollectionCallback extends EditCallback
     public void save(PersistenceService persistenceService, Object newObject)
     {
         executeOgnlExpression(collectionDescriptor.findAddExpression(), newObject);
-        persistenceService.save(getModel());
+        if ( isChildRelationship() ) {
+            persistenceService.merge(getModel());
+        } else {
+            persistenceService.save(getModel());
+        }
     }
-    
+
     public void remove(PersistenceService persistenceService, Object object)
     {
         executeOgnlExpression(collectionDescriptor.findRemoveExpression(), object);
-        persistenceService.save(getModel());
+        persistenceService.merge(getModel());
     }
-    
+
     /**
      * @param previousModel
      */
     private void executeOgnlExpression(String ognlExpression, Object newObject)
     {
-        HashMap context = new HashMap();
+        Map<String,Object> context = new HashMap<String,Object>();
         context.put("member", newObject);
 
         try
@@ -70,16 +75,16 @@ public class CollectionCallback extends EditCallback
             throw new TrailsRuntimeException(e);
         }
     }
-    
+
     public boolean isChildRelationship()
     {
         return childRelationship;
     }
-    
+
 
     public void setChildRelationship(boolean child)
     {
         this.childRelationship = child;
     }
-	
+
 }
