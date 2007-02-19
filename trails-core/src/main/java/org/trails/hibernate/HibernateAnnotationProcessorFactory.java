57eecbcb8951681e132fdec291f61a35942ced52
nhhockeyplayer <nhhockeyplayer@unknown.com>
2007-02-19 15:57:26 +0000

    Modified XPATH package parser to org.trails.* in order to accommodate other intrinsics as opposed to being exclusive to only security intrinsics

--- trails-core/src/main/java/org/trails/hibernate/HibernateAnnotationProcessorFactory.java
+++ trails-core/src/main/java/org/trails/hibernate/HibernateAnnotationProcessorFactory.java
@@ -22,87 +22,92 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-public class HibernateAnnotationProcessorFactory implements AnnotationProcessorFactory
-{
-	public static final String SECURITY_PACKAGE = "org.trails.security";
-	
-	public static final String configFileOption = "configFile";
-	
-	public static final String destFileOption = "destFile";
-	
-    public HibernateAnnotationProcessorFactory()
-    {
+public class HibernateAnnotationProcessorFactory implements
+        AnnotationProcessorFactory {
+    public static final String TRAILS_PACKAGE = "org.trails";
+
+    public static final String configFileOption = "configFile";
+
+    public static final String destFileOption = "destFile";
+
+    public HibernateAnnotationProcessorFactory() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    public Collection<String> supportedOptions()
-    {
+    public Collection<String> supportedOptions() {
         return null;
     }
 
-    public Collection<String> supportedAnnotationTypes()
-    {
+    public Collection<String> supportedAnnotationTypes() {
         return Arrays.asList("javax.persistence.Entity");
     }
 
-    public AnnotationProcessor getProcessorFor(Set<AnnotationTypeDeclaration> decls, AnnotationProcessorEnvironment env)
-    {
+    public AnnotationProcessor getProcessorFor(
+            Set<AnnotationTypeDeclaration> decls,
+            AnnotationProcessorEnvironment env) {
         return new HibernateAnnotationProcessor(env, decls);
     }
-    
-    public class HibernateAnnotationProcessor implements AnnotationProcessor
-    {
+
+    public class HibernateAnnotationProcessor implements AnnotationProcessor {
         private AnnotationProcessorEnvironment env;
+
         private Set<AnnotationTypeDeclaration> annotationTypeDeclarations;
-        
-        public HibernateAnnotationProcessor(
-                AnnotationProcessorEnvironment env, 
-                Set<AnnotationTypeDeclaration> annTypeDecls)
-        {
+
+        public HibernateAnnotationProcessor(AnnotationProcessorEnvironment env,
+                Set<AnnotationTypeDeclaration> annTypeDecls) {
             this.env = env;
             this.annotationTypeDeclarations = annTypeDecls;
         }
-        
+
         Element sessionFactoryElement;
-        
-        public void process()
-        {
-        	String configTemplateFilePath = getOptionValue(configFileOption);
-            
+
+        public void process() {
+            String configTemplateFilePath = getOptionValue(configFileOption);
+
             System.out.println(configTemplateFilePath);
-            try
-            {
+            try {
                 SAXReader reader = new SAXReader();
                 reader.setValidation(false);
                 reader.setEntityResolver(new DTDEntityResolver());
                 reader.setIncludeExternalDTDDeclarations(false);
                 reader.setIncludeInternalDTDDeclarations(false);
-                // Create file first, this is more reliable if there are spaces in the path
+                // Create file first, this is more reliable if there are spaces
+                // in the path
                 File configTemplateFile = new File(configTemplateFilePath);
                 Document doc = reader.read(configTemplateFile);
-                sessionFactoryElement = (Element)doc.getRootElement().selectSingleNode("//session-factory");
-                for (Iterator iter = sessionFactoryElement.selectNodes("mapping[not(starts-with(@class, '" + SECURITY_PACKAGE + "'))]").iterator(); iter.hasNext();)
-                {
+
+                sessionFactoryElement = (Element) doc.getRootElement()
+                        .selectSingleNode("//session-factory");
+
+                String trailsXPath = "mapping[not(starts-with(@class, '"
+                        + TRAILS_PACKAGE + "'))]";
+                for (Iterator iter = sessionFactoryElement.selectNodes(
+                        trailsXPath).iterator(); iter.hasNext();) {
                     Element element = (Element) iter.next();
+
                     sessionFactoryElement.remove(element);
                 }
-                List listenerElements = sessionFactoryElement.elements("listener");
+
+                List listenerElements = sessionFactoryElement
+                        .elements("listener");
                 sessionFactoryElement.elements().removeAll(listenerElements);
-                
-                for (AnnotationTypeDeclaration annotationTypeDecl : annotationTypeDeclarations)
-                {
+
+                for (AnnotationTypeDeclaration annotationTypeDecl : annotationTypeDeclarations) {
                     System.out.println(annotationTypeDecl);
-                    for (Declaration declaration : env.getDeclarationsAnnotatedWith(annotationTypeDecl))
-                    {
-                        declaration.accept(new SimpleDeclarationVisitor() 
-                        {
-                            public void visitClassDeclaration(ClassDeclaration classDeclaration)
-                            {
-                                System.out.println(classDeclaration.getQualifiedName());
-                                sessionFactoryElement.addElement("mapping").setAttributeValue("class", 
-                                        classDeclaration.getQualifiedName());
-                            }                       
+                    for (Declaration declaration : env
+                            .getDeclarationsAnnotatedWith(annotationTypeDecl)) {
+                        declaration.accept(new SimpleDeclarationVisitor() {
+                            public void visitClassDeclaration(
+                                    ClassDeclaration classDeclaration) {
+                                System.out.println(classDeclaration
+                                        .getQualifiedName());
+                                sessionFactoryElement.addElement("mapping")
+                                        .setAttributeValue(
+                                                "class",
+                                                classDeclaration
+                                                        .getQualifiedName());
+                            }
                         });
                     }
                     sessionFactoryElement.elements().addAll(listenerElements);
@@ -116,26 +121,22 @@ public class HibernateAnnotationProcessorFactory implements AnnotationProcessorF
                 XMLWriter writer = new XMLWriter(new PrintWriter(f), format);
                 writer.write(doc);
                 writer.close();
-                
-            }
-            catch (Exception ex) {
+
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
 
         }
 
-		private String getOptionValue(String option)
-		{
-			// This is a hack due to a bug in apt
-            for (String key : env.getOptions().keySet())
-			{
-				if (key.startsWith("-A" + option))
-				{
-					return key.split("=")[1];
-				}
-			}
-			return null;
-		}
-        
+        private String getOptionValue(String option) {
+            // This is a hack due to a bug in apt
+            for (String key : env.getOptions().keySet()) {
+                if (key.startsWith("-A" + option)) {
+                    return key.split("=")[1];
+                }
+            }
+            return null;
+        }
+
     }
 }
