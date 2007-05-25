15c6ca492e93b5ac6f1706c79534c3f9ec8e016e
Alejandro Scandroli <ascandroli@codehaus.org>
2007-05-25 17:27:35 +0000

    I forgot to check in the model for the tests

---
+++ trails-hibernate/src/test/java/org/trails/testhibernate/FooTest.java
@@ -0,0 +1,23 @@
+package org.trails.testhibernate;
+
+import junit.framework.TestCase;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+public class FooTest extends TestCase {
+
+    public void testOrder() throws Exception {
+        Foo foo = new Foo();
+        for (Method fooMethod : Foo.class.getDeclaredMethods()) {
+            //System.out.println(fooMethod.getName());
+
+        }
+        BeanInfo beanInfo = Introspector.getBeanInfo(Foo.class);
+        for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+            //System.out.println(descriptor.getName());
+        }
+    }
+}
