59b36514e5d976e25f99d373d26e6aea5d361f94
Kalle Korhonen <kaosko@apache.org>
2007-06-17 05:50:28 +0000

    Throw a security exception in alteCriteria() if there's no suitable role and no association permission

--- trails-security/src/main/java/org/trails/hibernate/SecurePersistenceServiceImpl.java
+++ trails-security/src/main/java/org/trails/hibernate/SecurePersistenceServiceImpl.java
@@ -8,14 +8,13 @@ import org.acegisecurity.context.SecurityContextHolder;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
 import org.hibernate.criterion.SimpleExpression;
+import org.trails.security.EntitySecurityException;
 import org.trails.security.annotation.ViewRequiresAssociation;
 import org.trails.security.annotation.ViewRequiresRole;
 
 public class SecurePersistenceServiceImpl extends HibernatePersistenceServiceImpl {
 	@Override
 	protected DetachedCriteria alterCriteria(Class type, DetachedCriteria criteria) {
-		ViewRequiresAssociation viewRestriction = (ViewRequiresAssociation)type.getAnnotation(ViewRequiresAssociation.class );
-		if (viewRestriction == null) return criteria;
 		SecurityContext context = SecurityContextHolder.getContext();
 		// Assume that context should have been established for each request, and if it's not,
 		// it's an internal service call 
@@ -25,6 +24,16 @@ public class SecurePersistenceServiceImpl extends HibernatePersistenceServiceImp
 		ViewRequiresRole viewRoleRestriction = (ViewRequiresRole)type.getAnnotation(ViewRequiresRole.class );
 		if (viewRoleRestriction != null) for (GrantedAuthority authority : context.getAuthentication().getAuthorities())
 			for (String role : viewRoleRestriction.value()) if (authority.getAuthority().equals(role) ) return criteria;
+
+		ViewRequiresAssociation viewRestriction = (ViewRequiresAssociation)type.getAnnotation(ViewRequiresAssociation.class );
+		
+		if (viewRoleRestriction == null && viewRestriction == null) return criteria;
+		
+		if (viewRestriction == null) {
+			// At this point we know the user should have no access to the entities, because there's
+			// a role restriction but user doesn't have a suitable role and there's no association
+			throw new EntitySecurityException(null, "No suitable role or association");
+		}
 		
 		String currentUsername = context.getAuthentication().getName();
 		String ownerPropertyAssociation = viewRestriction.value();
