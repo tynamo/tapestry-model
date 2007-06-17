ce2abd2879b30496dfc9e82e6fa4633d7962c32a
Alejandro Scandroli <ascandroli@codehaus.org>
2007-06-17 10:33:19 +0000

    fix TRAILS-66

---
+++ modules/trails-security/src/main/java/org/trails/page/LoginPage.java
@@ -0,0 +1,26 @@
+package org.trails.page;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.RedirectException;
+import org.apache.tapestry.html.BasePage;
+
+public abstract class LoginPage extends BasePage {
+
+	private static final Log LOG = LogFactory.getLog(LoginPage.class);
+
+	public abstract String getUsername();
+
+	public abstract String getPassword();
+
+	public void login(IRequestCycle cycle) throws RedirectException {
+
+		LOG.debug("User " + getUsername() + " is attempting login.");
+
+		String acegiUrl = cycle.getAbsoluteURL("/j_acegi_security_check?j_username=" + getUsername() + "&j_password=" + getPassword());
+
+		throw new RedirectException(acegiUrl);
+	}
+
+}
