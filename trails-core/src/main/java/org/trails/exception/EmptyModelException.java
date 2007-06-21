3927ff28651b6baf5d51a667dd4b8a47e53319c3
Kalle Korhonen <kaosko@apache.org>
2007-06-21 18:04:37 +0000

    Add EmptyModelException and throw on ModelPage if model is null

---
+++ trails-core/src/main/java/org/trails/exception/EmptyModelException.java
@@ -0,0 +1,11 @@
+package org.trails.exception;
+
+import org.trails.TrailsRuntimeException;
+
+public class EmptyModelException extends TrailsRuntimeException {
+
+	public EmptyModelException(String message) {
+		super(message);
+	}
+
+}
