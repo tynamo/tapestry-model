f98e690d4271722dc5ed0194a913bf9bf2babdbe
nhhockeyplayer <nhhockeyplayer@unknown.com>
2007-09-24 16:52:02 +0000

    HardOneToOne   -  moved annotations to ROSTER Demo   -  this gives good example of how to roll and smoke      your own annotations

---
+++ examples/roster/src/main/java/org/trails/descriptor/annotation/HardOneToOneAnnotationHandler.java
@@ -0,0 +1,31 @@
+package org.trails.descriptor.annotation;
+
+import org.trails.descriptor.IPropertyDescriptor;
+import org.trails.descriptor.OwningObjectReferenceDescriptor;
+
+public class HardOneToOneAnnotationHandler extends AbstractAnnotationHandler implements
+		DescriptorAnnotationHandler<HardOneToOne, IPropertyDescriptor>
+{
+
+	public HardOneToOneAnnotationHandler()
+	{
+		super();
+	}
+
+	public IPropertyDescriptor decorateFromAnnotation(HardOneToOne propertyDescriptorAnno,
+			IPropertyDescriptor descriptorReference)
+	{
+		if (propertyDescriptorAnno.identity() == org.trails.descriptor.annotation.HardOneToOne.Identity.OWNER)
+		{
+			OwningObjectReferenceDescriptor owningObjectReferenceDescriptor = new OwningObjectReferenceDescriptor();
+
+			Class type = descriptorReference.getPropertyType();
+			String inverseProperty = descriptorReference.getName();
+			owningObjectReferenceDescriptor.setInverseProperty(inverseProperty);
+			setPropertiesFromAnnotation(propertyDescriptorAnno, owningObjectReferenceDescriptor);
+			descriptorReference.addExtension(OwningObjectReferenceDescriptor.class.getName(),
+					owningObjectReferenceDescriptor);
+		}
+		return descriptorReference;
+	}
+}
