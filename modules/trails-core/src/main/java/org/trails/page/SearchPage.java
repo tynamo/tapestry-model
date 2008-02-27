0f7c87173b0e3fb17b9e65e86b05f07ce30faed7
Alejandro Scandroli <ascandroli@codehaus.org>
2008-02-27 21:44:50 +0000

    removing obsolete code, I should have done this when I fixed TRAILS-80

--- modules/trails-core/src/main/java/org/trails/page/SearchPage.java
+++ modules/trails-core/src/main/java/org/trails/page/SearchPage.java
@@ -11,22 +11,6 @@ import java.util.List;
 public abstract class SearchPage extends TrailsPage
 {
 
-	public String[] getSearchableProperties()
-	{
-		try
-		{
-			//@note: Danger: gross code
-			ArrayList<String> stringList = new ArrayList<String>();
-			stringList.addAll(
-					(List) Ognl.getValue("propertyDescriptors.{? searchable}.{name}", getClassDescriptor()));
-			return stringList.toArray(new String[]{});
-		}
-		catch (OgnlException oe)
-		{
-			throw new TrailsRuntimeException(oe);
-		}
-	}
-
 	public void activateTrailsPage(Object[] objects, IRequestCycle iRequestCycle)
 	{
 		if (getCallbackStack() != null)
