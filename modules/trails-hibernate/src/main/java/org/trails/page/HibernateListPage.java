fab71fbd78c597cc6d9913f1189e5d53c61ad759
Alejandro Scandroli <ascandroli@codehaus.org>
2008-03-09 15:14:00 +0000

    allowing pages that extend from Hibernate pages, access to the HibernatePersistenceService

--- modules/trails-hibernate/src/main/java/org/trails/page/HibernateListPage.java
+++ modules/trails-hibernate/src/main/java/org/trails/page/HibernateListPage.java
@@ -1,11 +1,23 @@
 package org.trails.page;
 
-import org.apache.tapestry.IExternalPage;
 import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.InjectObject;
 import org.hibernate.criterion.DetachedCriteria;
+import org.trails.persistence.HibernatePersistenceService;
 
 public abstract class HibernateListPage extends ListPage
 {
+
+	@InjectObject("service:trails.hibernate.PersistenceService")
+	public abstract HibernatePersistenceService getHibernatePersistenceService();
+
+	/**
+	 * @todo: remove when the components reuse issue goes away.
+	 */
+	public HibernatePersistenceService getPersistenceService()
+	{
+		return getHibernatePersistenceService();
+	}
 
 	public void activateTrailsPage(Object[] args, IRequestCycle cycle)
 	{
