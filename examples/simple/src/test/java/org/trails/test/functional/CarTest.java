3fe96b42322180c597a276610d5432f1337d9589
Alejandro Scandroli <ascandroli@codehaus.org>
2008-03-09 12:30:52 +0000

    functional test case for TRAILS-145 and TRAILS-144

--- examples/simple/src/test/java/org/trails/test/functional/CarTest.java
+++ examples/simple/src/test/java/org/trails/test/functional/CarTest.java
@@ -29,4 +29,25 @@ public class CarTest extends FunctionalTest
 		HtmlPage make1Page = clickLinkOnPage(listMakesPage, "1");
 		assertXPathPresent(make1Page, "//td[text() = 'Accord']");
 	}
+
+	public void testCancelAndDefaultCallback() throws Exception {
+
+		HtmlPage listMakesPage = clickLinkOnPage(startPage, "List Makes");
+		HtmlPage newMakePage = clickLinkOnPage(listMakesPage, "New Make");
+
+		newMakePage = clickButton(newMakePage, "Cancel");
+		assertXPathPresent(newMakePage, "//h1[text() = 'List Makes ']"); //note there an extra space in the h1 text
+	}
+
+	public void testCancelWithSessionCallbackStack() throws Exception
+	{
+		HtmlPage listMakesPage = clickLinkOnPage(startPage, "List Makes");
+		HtmlPage listCarsPage = clickLinkOnPage(startPage, "List Cars");
+
+		HtmlPage newMakePage = clickLinkOnPage(listMakesPage, "New Make"); //this is to add a callback pointing to the New Make page to the callbackStack
+		HtmlPage newCarPage = clickLinkOnPage(listCarsPage, "New Car");
+
+		newMakePage = clickButton(newCarPage, "Cancel");
+		assertXPathPresent(newMakePage, "//h1[text() = 'Edit Make']");
+	}
 }
