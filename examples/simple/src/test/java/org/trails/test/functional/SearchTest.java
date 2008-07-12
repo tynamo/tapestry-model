bb57528878f15c19d69ba31947150bee56b21b08
Alejandro Scandroli <ascandroli@codehaus.org>
2008-07-12 08:30:09 +0000

    fixed TRAILS-60 and its duplicate TRAILS-160 applied Pablo Graña's patch. Thanks Pablo.

--- examples/simple/src/test/java/org/trails/test/functional/SearchTest.java
+++ examples/simple/src/test/java/org/trails/test/functional/SearchTest.java
@@ -1,6 +1,7 @@
 package org.trails.test.functional;
 
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 
 public class SearchTest extends FunctionalTest
 {
@@ -9,6 +10,9 @@ public class SearchTest extends FunctionalTest
 		HtmlPage listApplesPage = clickLinkOnPage(startPage, "List Apples");
 		HtmlPage newApplePage = clickLinkOnPage(listApplesPage, "New Apple");
 		getInputByName(newApplePage, "Color").setValueAttribute("Blue");
+		HtmlSelect makeSelect = getSelectByName(newApplePage, "Origin");
+		makeSelect.setSelectedAttribute("AMERICA", true);
+
 		listApplesPage = clickButton(newApplePage, "Ok");
 		HtmlPage searchApplesPage = clickLinkOnPage(listApplesPage, "Search Apples");
 		getInputByName(searchApplesPage, "Color").setValueAttribute("Blue");
@@ -18,5 +22,31 @@ public class SearchTest extends FunctionalTest
 		getInputByName(searchApplesPage, "Color").setValueAttribute("lu");
 		listApplesPage = clickButton(searchApplesPage, "Search");
 		assertXPathPresent(listApplesPage, "//td[text() = 'Blue']");
+	}
+
+	public void testSearchByEnum() throws Exception
+	{
+		HtmlPage listApplesPage = clickLinkOnPage(startPage, "List Apples");
+		HtmlPage newApplePage = clickLinkOnPage(listApplesPage, "New Apple");
+		getInputByName(newApplePage, "Color").setValueAttribute("Red");
+		HtmlSelect makeSelect = getSelectByName(newApplePage, "Origin");
+		makeSelect.setSelectedAttribute("OCEANIA", true);
+		listApplesPage = clickButton(newApplePage, "Ok");
+
+		HtmlPage searchApplesPage = clickLinkOnPage(listApplesPage, "Search Apples");
+		makeSelect = getSelectByName(searchApplesPage, "Origin");
+		makeSelect.setSelectedAttribute("OCEANIA", true);
+
+		listApplesPage = clickButton(searchApplesPage, "Search");
+		assertXPathPresent(listApplesPage, "//td[text() = 'Red']");
+		assertXPathNotPresent(listApplesPage, "//td[text() = 'Blue']");
+
+		searchApplesPage = clickLinkOnPage(listApplesPage, "Search Apples");
+		makeSelect = getSelectByName(searchApplesPage, "Origin");
+		makeSelect.setSelectedAttribute("EUROPE", true);
+
+		listApplesPage = clickButton(searchApplesPage, "Search");
+		assertXPathNotPresent(listApplesPage, "//td[text() = 'Blue']");
+		assertXPathNotPresent(listApplesPage, "//td[text() = 'Red']");
 	}
 }
