4e99b0cc89cfc3a02d656668d66002d3430ee0d0
Alejandro Scandroli <ascandroli@codehaus.org>
2008-08-11 17:19:32 +0000

    TRAILS-84 refactoring the code provided in the patch

--- modules/trails-core/src/main/java/org/trails/descriptor/annotation/PossibleValuesDescriptorExtension.java
+++ modules/trails-core/src/main/java/org/trails/descriptor/annotation/PossibleValuesDescriptorExtension.java
@@ -1,49 +1,17 @@
 package org.trails.descriptor.annotation;
 
-import org.apache.commons.lang.Validate;
-import org.trails.descriptor.IDescriptorExtension;
-
 /**
  * Extension to the property descriptor that holds information about possible
  * values.
  *
  * @author pruggia
  */
-public class PossibleValuesDescriptorExtension implements IDescriptorExtension
+public class PossibleValuesDescriptorExtension extends ExpressionExtension
 {
 
-	/**
-	 * The serial id.
-	 */
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Ognl expression that evaluated gets a list of possible values to use with
-	 * the current property, cannot be null.
-	 */
-	private String expression;
-
-	/**
-	 * Creates a {@link PossibleValuesDescriptorExtension}.
-	 *
-	 * @param theExpression Ognl expression that evaluated gets a list of possible
-	 *                      values to use with the current property, cannot be null.
-	 */
-	public PossibleValuesDescriptorExtension(final String theExpression)
+	public PossibleValuesDescriptorExtension(String theExpression)
 	{
-		super();
-		Validate.notNull(theExpression, "The expression cannot be null");
-		expression = theExpression;
+		super(theExpression);
 	}
 
-	/**
-	 * Gets the Ognl expression that evaluated gets a list of possible values to
-	 * use with the current property.
-	 *
-	 * @return a String, never null.
-	 */
-	public String getExpression()
-	{
-		return expression;
-	}
 }
