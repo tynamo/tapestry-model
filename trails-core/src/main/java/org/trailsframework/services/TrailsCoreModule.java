3989aea8f3d8c198af91f6b08e770ad985510b10
Kalle Korhonen <kaosko@apache.org>
2009-02-03 07:26:31 +0000

    bind DescriptorService

--- trails-core/src/main/java/org/trailsframework/services/TrailsCoreModule.java
+++ trails-core/src/main/java/org/trailsframework/services/TrailsCoreModule.java
@@ -9,15 +9,16 @@ import org.apache.tapestry5.ioc.services.CoercionTuple;
 import org.apache.tapestry5.services.BeanBlockContribution;
 import org.apache.tapestry5.services.DataTypeAnalyzer;
 import org.apache.tapestry5.services.LibraryMapping;
-import org.trailsframework.descriptor.*;
+import org.trailsframework.descriptor.DescriptorFactory;
+import org.trailsframework.descriptor.MethodDescriptorFactory;
+import org.trailsframework.descriptor.MethodDescriptorFactoryImpl;
+import org.trailsframework.descriptor.PropertyDescriptorFactory;
+import org.trailsframework.descriptor.PropertyDescriptorFactoryImpl;
+import org.trailsframework.descriptor.ReflectionDescriptorFactory;
 
-import java.util.Collection;
+public class TrailsCoreModule {
 
-public class TrailsCoreModule
-{
-
-	public static void bind(ServiceBinder binder)
-	{
+	public static void bind(ServiceBinder binder) {
 		// Make bind() calls on the binder object to define most IoC services.
 		// Use service builder methods (example below) when the implementation
 		// is provided inline, or requires more initialization than simply
@@ -27,22 +28,21 @@ public class TrailsCoreModule
 		binder.bind(PropertyDescriptorFactory.class, PropertyDescriptorFactoryImpl.class);
 		binder.bind(MethodDescriptorFactory.class, MethodDescriptorFactoryImpl.class);
 		binder.bind(EntityCoercerService.class, EntityCoercerServiceImpl.class);
+		binder.bind(DescriptorService.class, DescriptorServiceImpl.class);
 
 	}
 
 	/**
 	 * Add our components and pages to the "trails" library.
 	 */
-	public static void contributeComponentClassResolver(Configuration<LibraryMapping> configuration)
-	{
+	public static void contributeComponentClassResolver(Configuration<LibraryMapping> configuration) {
 		configuration.add(new LibraryMapping("trails", "org.trailsframework"));
 	}
 
 	/**
 	 * Contribution to the BeanBlockSource service to tell the BeanEditForm component about the editors.
 	 */
-	public static void contributeBeanBlockSource(Configuration<BeanBlockContribution> configuration)
-	{
+	public static void contributeBeanBlockSource(Configuration<BeanBlockContribution> configuration) {
 		configuration.add(new BeanBlockContribution("dateEditor", "trails/Editors", "date", true));
 		configuration.add(new BeanBlockContribution("fckEditor", "trails/Editors", "fckEditor", true));
 		configuration.add(new BeanBlockContribution("readOnly", "trails/Editors", "readOnly", true));
@@ -52,18 +52,18 @@ public class TrailsCoreModule
 		configuration.add(new BeanBlockContribution("editComposition", "trails/Editors", "editComposition", true));
 	}
 
-
 	/**
-	 * <dl> <dt>Annotation</dt> <dd>Checks for {@link org.apache.tapestry5.beaneditor.DataType} annotation</dd>
-	 * <dt>Default  (ordered last)</dt> <dd>{@link org.apache.tapestry5.internal.services.DefaultDataTypeAnalyzer}
-	 * service ({@link #contributeDefaultDataTypeAnalyzer(org.apache.tapestry5.ioc.MappedConfiguration)} })</dd> </dl>
+	 * <dl>
+	 * <dt>Annotation</dt>
+	 * <dd>Checks for {@link org.apache.tapestry5.beaneditor.DataType} annotation</dd>
+	 * <dt>Default (ordered last)</dt>
+	 * <dd>{@link org.apache.tapestry5.internal.services.DefaultDataTypeAnalyzer} service (
+	 * {@link #contributeDefaultDataTypeAnalyzer(org.apache.tapestry5.ioc.MappedConfiguration)} )</dd>
+	 * </dl>
 	 */
 	public static void contributeDataTypeAnalyzer(OrderedConfiguration<DataTypeAnalyzer> configuration,
-												  @InjectService("DefaultDataTypeAnalyzer")
-												  DataTypeAnalyzer defaultDataTypeAnalyzer,
-												  @InjectService("TrailsDataTypeAnalyzer")
-												  DataTypeAnalyzer trailsDataTypeAnalyzer)
-	{
+			@InjectService("DefaultDataTypeAnalyzer") DataTypeAnalyzer defaultDataTypeAnalyzer,
+			@InjectService("TrailsDataTypeAnalyzer") DataTypeAnalyzer trailsDataTypeAnalyzer) {
 		configuration.add("Trails", trailsDataTypeAnalyzer);
 		configuration.add("Default", defaultDataTypeAnalyzer, "after:*");
 	}
@@ -76,14 +76,12 @@ public class TrailsCoreModule
 	 * </ul>
 	 */
 	public static void contributeTypeCoercer(final Configuration<CoercionTuple> configuration,
-											 @InjectService("EntityCoercerService") EntityCoercerService entityCoercerService)
-	{
+			@InjectService("EntityCoercerService") EntityCoercerService entityCoercerService) {
 		configuration.add(new CoercionTuple<Class, String>(Class.class, String.class, new ClassToStringCoercion(entityCoercerService)));
 		configuration.add(new CoercionTuple<String, Class>(String.class, Class.class, new StringToClassCoercion(entityCoercerService)));
 	}
 
-	public static TrailsDataTypeAnalyzer buildTrailsDataTypeAnalyzer(ServiceResources resources)
-	{
+	public static TrailsDataTypeAnalyzer buildTrailsDataTypeAnalyzer(ServiceResources resources) {
 		return resources.autobuild(TrailsDataTypeAnalyzer.class);
 	}
 
