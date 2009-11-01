1f760f0fac11e2204299263950ad7765862620bf
Alejandro Scandroli <ascandroli@codehaus.org>
2009-11-01 07:50:31 +0000

    exclude collections from the grid beanModel

--- tynamo-crud-hibernate/src/main/java/org/tynamo/hibernate/pages/HibernateListPage.java
+++ tynamo-crud-hibernate/src/main/java/org/tynamo/hibernate/pages/HibernateListPage.java
@@ -1,36 +1,19 @@
 package org.tynamo.hibernate.pages;
 
-import ognl.Ognl;
-import ognl.OgnlException;
-import org.apache.commons.lang.builder.StandardToStringStyle;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.beaneditor.BeanModel;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.services.PropertyAccess;
+import org.apache.tapestry5.services.BeanModelSource;
 import org.tynamo.descriptor.TynamoClassDescriptor;
 import org.tynamo.services.DescriptorService;
 import org.tynamo.services.PersistenceService;
+import org.tynamo.util.BeanModelUtils;
 import org.tynamo.util.DisplayNameUtils;
 
 public class HibernateListPage
 {
-	final static ToStringStyle commaSeparatedListStyle = new StandardToStringStyle();
-
-	static
-	{
-		StandardToStringStyle styleValues = (StandardToStringStyle) HibernateListPage.commaSeparatedListStyle;
-
-		styleValues.setUseClassName(false);
-		styleValues.setUseIdentityHashCode(false);
-		styleValues.setUseFieldNames(false);
-		styleValues.setContentStart("");
-		styleValues.setContentEnd("");
-		styleValues.setArrayStart("");
-		styleValues.setArrayEnd("");
-		styleValues.setArraySeparator(",");
-	}
 
 	@Inject
 	private PersistenceService persitenceService;
@@ -44,15 +27,21 @@ public class HibernateListPage
 	@Inject
 	private PropertyAccess propertyAccess;
 
-	@Property
-	private Object model;
+	@Inject
+	private BeanModelSource beanModelSource;
 
-	@Property(write = false)
+	@Property
+	private Object bean;
+
 	private TynamoClassDescriptor classDescriptor;
+
+	private BeanModel beanModel;
 
 	protected void onActivate(Class clazz) throws Exception
 	{
 		classDescriptor = descriptorService.getClassDescriptor(clazz);
+		beanModel = beanModelSource.createDisplayModel(clazz, messages);
+		BeanModelUtils.exclude(beanModel, classDescriptor);
 	}
 
 	protected Object[] onPassivate()
@@ -67,12 +56,13 @@ public class HibernateListPage
 
 	public Object[] getEditPageContext()
 	{
-		return new Object[]{classDescriptor.getType(), model};
+		return new Object[]{classDescriptor.getType(), bean};
 	}
 
 	public String getTitle()
 	{
-		return messages.format("org.tynamo.i18n.list", DisplayNameUtils.getPluralDisplayName(classDescriptor, messages));
+		return messages
+				.format("org.tynamo.i18n.list", DisplayNameUtils.getPluralDisplayName(classDescriptor, messages));
 	}
 
 	public String getNewLinkMessage()
@@ -82,26 +72,17 @@ public class HibernateListPage
 
 	public final String getModelId()
 	{
-		return propertyAccess.get(model, classDescriptor.getIdentifierDescriptor().getName()).toString();
+		return propertyAccess.get(bean, classDescriptor.getIdentifierDescriptor().getName()).toString();
 	}
 
-	/**
-	 * @return A comma-separated list of property names to be removed from the Grid BeanModel
-	 */
-	public String getExcludeList()
+
+	public final TynamoClassDescriptor getClassDescriptor()
 	{
-		try
-		{
-			/* I'm sorry about this code, but it was the fastest way */
-			java.util.List<String> names = (java.util.List<String>) Ognl
-					.getValue("propertyDescriptors.{? identifier or !summary or hidden}.{name}", classDescriptor);
+		return classDescriptor;
+	}
 
-			return new ToStringBuilder(names, commaSeparatedListStyle).append(names.toArray()).toString();
-
-		} catch (OgnlException e)
-		{
-		}
-		
-		return "";
+	public final BeanModel getBeanModel()
+	{
+		return beanModel;
 	}
 }
