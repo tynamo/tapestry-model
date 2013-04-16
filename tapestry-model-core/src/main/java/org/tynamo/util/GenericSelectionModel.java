1bf8fa901020e6101f92e2d86864ba11565d0b63
Alejandro Scandroli <ascandroli@codehaus.org>
2013-04-16 20:39:56 +0000

    fixes TYNAMO-33 - The elements in the SelectModel should be sorted by label

--- tapestry-model-core/src/main/java/org/tynamo/util/GenericSelectionModel.java
+++ tapestry-model-core/src/main/java/org/tynamo/util/GenericSelectionModel.java
@@ -7,35 +7,40 @@ import org.apache.tapestry5.ioc.services.PropertyAccess;
 import org.apache.tapestry5.util.AbstractSelectModel;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class GenericSelectionModel<T> extends AbstractSelectModel
 {
 
 	private final String labelField;
-
 	private final List<T> list;
-
 	private final PropertyAccess adapter;
+	private final boolean sortByLabel;
 
 	public GenericSelectionModel(List<T> list)
 	{
-		this.labelField = null;
-		this.list = list;
-		this.adapter = null;
+		this(list, null, null, true);
 	}
 
-	public GenericSelectionModel(List<T> list, String labelField, PropertyAccess adapter)
+	public GenericSelectionModel(List<T> list, boolean sortByLabel)
+	{
+		this(list, null, null, sortByLabel);
+	}
+
+
+	public GenericSelectionModel(List<T> list, String labelField, PropertyAccess adapter, boolean sortByLabel)
 	{
 		this.labelField = labelField;
 		this.list = list;
 		this.adapter = adapter;
+		this.sortByLabel = sortByLabel;
 	}
 
 	public List<OptionGroupModel> getOptionGroups()
 	{
-		return Collections.EMPTY_LIST;
+		return Collections.emptyList();
 	}
 
 	public List<OptionModel> getOptions()
@@ -51,6 +56,18 @@ public class GenericSelectionModel<T> extends AbstractSelectModel
 				optionModelList.add(new OptionModelImpl(adapter.get(obj, labelField).toString(), obj));
 			}
 		}
+
+		if (sortByLabel)
+		{
+			Collections.sort(optionModelList, new Comparator<OptionModel>()
+			{
+				public int compare(OptionModel o1, OptionModel o2)
+				{
+					return o1.getLabel().compareTo(o2.getLabel());
+				}
+			});
+		}
+
 		return optionModelList;
 	}
 }
